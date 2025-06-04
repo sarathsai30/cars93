c332867f638e6f20c8fb4fb0985bdd409fb6e84e
Nagendra9398 <nagendravarmakeerthipati@gmail.com>
2025-06-04 22:46:47 +0530

    added search components

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ import Promo from './Modules/Promo/Promo.jsx';
 import AboutUs from './Modules/AboutUs/AboutUs.jsx';
 import FeaturedCars from './Modules/FeaturedCars/FeaturedCars.jsx';
 import TopSellingCars from './Modules/TopSellingCars/TopSellingCars.jsx';
+import Search from './Modules/Search/Search.jsx';
+import SearchBar from'./Modules/SearchBar/SearchBar.jsx';
+
 
 function App() {
   return (
     <div className="container-fluid">
       <div className="d-flex flex-column min-vh-100">
         <Header />
-        <Promo />
+        {/* Top-right input search*/}
+        <SearchBar />
+      <div style={{position:"relative"}}>
+        <Promo />{/*promo should include the car image*/}
+        <div style={{
+          position: "absolute",
+          top: "40%",    // Adjust vertically
+          left: "10%",  // Adjust horizontally
+          transform: "translate(-50%, -50%)",
+          zindex:10
+        }}>
+          <Search />
+        </div>
+      </div>
         <FeaturedCars />
         {/* <TopSellingCars /> */}
         <main className="flex-grow-1">
